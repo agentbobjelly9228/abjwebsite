da2bce4da58a127bcfcdd4a1fe115770c853bb34
Alex Man <alexman@MacBook-Pro.local>
2020-08-25 23:07:02 -0700

    hi

--- script.js
+++
@@ -1,9 +0,0 @@
-btn = document.getElementById('login')
-btn.addEventListener("click", function() {
-  alert('')
-})
-
-class user(){
-  
-  
-}
---
+++ static/script.js
@@ -0,0 +1,5 @@
+btn = document.getElementById('test')
+btn.addEventListener("click", function() {
+  alert('test')
+})
+
